794b8e401cc59a508145a400fcae433a0a69442a
Seyfullah Cahit Sürücü <seyfocemo99@gmail.com>
2024-05-08 14:59:36 +0300

    Home Page Design

--- weather_app/lib/consts/consts.dart
+++ weather_app/lib/consts/consts.dart
@@ -1,3 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:weather_icons/weather_icons.dart';
 
+//Colors
 Color appBarbgColor = const Color.fromARGB(255, 82, 144, 252);
+Color bgColor = const Color.fromARGB(255, 35, 64, 114);
+
+// TextColors
+Color textColor = const Color.fromARGB(255, 255, 255, 255);
+
+//Icons
+IconData sunny = WeatherIcons.day_sunny;
+IconData rain = WeatherIcons.rain;
+IconData cloudy = WeatherIcons.cloudy;
+
+//IconColors
+Color iconColor = const Color.fromARGB(255, 255, 255, 255);
--- weather_app/lib/screens/home.dart
+++ weather_app/lib/screens/home.dart
@@ -13,6 +13,55 @@ class Home extends StatelessWidget {
           backgroundColor: appBarbgColor,
           title: const Text("Weather App"),
         ),
+        body: Container(
+          color: bgColor,
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  "City",
+                  style: TextStyle(fontSize: 28, color: textColor),
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Icon(
+                      color: iconColor,
+                      cloudy,
+                      size: 60,
+                    ),
+                    const SizedBox(
+                      width: 50,
+                    ),
+                    Text(
+                      "-1",
+                      style: TextStyle(fontSize: 30, color: textColor),
+                    ),
+                    const SizedBox(
+                      width: 20,
+                    ),
+                    Column(
+                      children: [
+                        Text(
+                          "Min : " + "data",
+                          style: TextStyle(color: textColor),
+                        ),
+                        Text(
+                          "Max : " + "data",
+                          style: TextStyle(color: textColor),
+                        ),
+                      ],
+                    )
+                  ],
+                )
+              ],
+            ),
+          ),
+        ),
       ),
     );
   }
